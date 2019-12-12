b1ed6214a3cb9f2599156f234b0fe8d54290c0b8
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-12 14:11:30 -0800

    Create recipe Router.

---
+++ recipes/recipeRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const recipeDb = require('./recipesDb');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    
+});
+
+router.get('/:id', (req, res) => {
+    
+});
+
+router.post('/', (req, res) => {
+    
+});
+
+router.put('/:id', (req, res) => {
+    
+});
+
+router.delete('/:id', (req, res) => {
+    
+});
+
+module.exports = router;
