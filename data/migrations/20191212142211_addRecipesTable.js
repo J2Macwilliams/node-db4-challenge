be4498a9563e8da47f17e1157b249f43077bf056
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-12 14:47:34 -0800

    Create recipe_details table.

--- data/migrations/20191212142211_addRecipesTable.js
+++ data/migrations/20191212142211_addRecipesTable.js
@@ -1,13 +1,39 @@
-
 exports.up = function(knex) {
-  return knex.schema.createTable('recipes', tbl => {
-      tbl.increments();
-      tbl.string('recipe_name', 128)
-  })
+	return knex.schema
+		.createTable('recipes', tbl => {
+			tbl.increments();
+			tbl.string('recipe_name', 128).notNullable();
+		})
 
+		.createTable('recipe_detail', tbl => {
+			tbl.primary(['recipes_id', 'ingredients_id', 'instruction_id']);
+			tbl
+				.integer('recipes_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('tickets');
 
+			tbl.string('name', 128).notNullable();
+
+			tbl
+				.integer('ingredients_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('employees');
+
+			tbl.float('quantity', 255).notNullable();
+
+			tbl.string('measurement_unit', 255).notNullable();
+
+			tbl
+				.integer('instructions_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('employees');
+		});
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function(knex) {};
