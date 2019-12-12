bb89023742e098ed39f326e102c97edc5441675c
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-12 14:39:45 -0800

    Create first table in migration.

--- data/migrations/20191212142211_addRecipesTable.js
+++ data/migrations/20191212142211_addRecipesTable.js
@@ -1,6 +1,11 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('recipes', tbl => {
+      tbl.increments();
+      tbl.string('recipe_name', 128)
+  })
+
+
 };
 
 exports.down = function(knex) {
